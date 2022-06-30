e63a216382f6500b0503568bef9149c3a504b074
yih8 <80453336+yih8@users.noreply.github.com>
2022-06-30 22:16:33 +0200

    linear eq ex

--- 模块3.py
+++ 模块3.py
@@ -1,5 +1,6 @@
 
 from sympy import *
-x = Symbol('x')
-y = Symbol('y')
-print(solve([3*x + 4*y - 49, 8*x - y - 14], [x, y]))
+x1 = Symbol('x1')
+x2 = Symbol('x2')
+x3 = Symbol('x3')
+print(solve([2*x1 + 3*x2 - 5*x3 + 10, 4*x1 + 8*x2 - 3*x3 + 19, -6*x1 + x2 + 4*x3 + 11 ], [x1,x2,x3]))
